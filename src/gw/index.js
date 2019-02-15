c3aec29fbc2e7b39cb122b17181c53647accd871
Nikita Korotkih <nkor@hellofresh.com>
2019-02-15 11:27:02 +0100

    verify token

--- src/gw/index.js
+++ src/gw/index.js
@@ -3,6 +3,8 @@ const { createServer } = require('../lib/server');
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
+const jwtSignKey = '123456'; // TODO hide secret
+
 createServer({
     port: 3000,
     srvName: 'GW',
@@ -16,6 +18,15 @@ createServer({
 
                 if (typeof authHeader !== 'undefined' && authRE.test(authHeader)) { // srv2
                     const token = authRE.exec(authHeader)[1];
+                    let userData;
+                    try {
+                        userData = jwt.verify(token, jwtSignKey);
+                    } catch (e) {
+                        console.log(e);
+
+                        return reply(new Error('invalid token'));
+                    }
+
                     const decoded = jwt.decode(token);
                     const { data: { message } } = await axios.get(`http://localhost:3002/hello?name=${decoded.name}`);
 
